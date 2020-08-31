1575cdf6d359b56ad096427c79ea0bcb27036569
OlgaBelogurova <obelogurova2@gmail.com>
2020-08-31 12:16:55 -0700

    Updates

---
+++ precache-manifest.5812a876ce8699107ed414d0043947b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e40b93ed92f4ef046b3ba131bf4acf1",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "af69fb9e55a6c10d2113",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea5cfc3202a04704897a",
+    "url": "/react-gh-pages/static/js/2.a51ad747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.a51ad747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af69fb9e55a6c10d2113",
+    "url": "/react-gh-pages/static/js/main.8c461a88.chunk.js"
+  },
+  {
+    "revision": "66e191b5fd3a61b4dd4a",
+    "url": "/react-gh-pages/static/js/runtime-main.732442c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
